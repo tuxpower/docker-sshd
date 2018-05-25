9679fd46795eee88fe7fd7e6f550abe301a9b898
Sarunas Krisciukaitis <sarunas.krisciukaitis@gmail.com>
2018-05-25 08:44:17 +0100

    Extending image to be able to use private key

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ ENV ROOT_PASSWORD root
 RUN apk update	&& apk upgrade && apk add openssh \
 		&& sed -i s/#PermitRootLogin.*/PermitRootLogin\ yes/ /etc/ssh/sshd_config \
 		&& echo "root:${ROOT_PASSWORD}" | chpasswd \
+		&& mkdir -m 0700 -p ~/.ssh \
 		&& rm -rf /var/cache/apk/* /tmp/*
 
 COPY entrypoint.sh /usr/local/bin/
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,12 @@ if [ ! -z "${ROOT_PASSWORD}" ] && [ "${ROOT_PASSWORD}" != "root" ]; then
     echo "root:${ROOT_PASSWORD}" | chpasswd
 fi
 
+# check wether a SSH key is provided
+if [ ! -z "${ROOT_PUBKEY}" ]; then
+    echo "${ROOT_PUBKEY}" > ~/.ssh/authorized_keys
+    chmod 400 ~/.ssh/authorized_keys
+fi
+
+
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 exec /usr/sbin/sshd -D -e "$@"
